462be50b51ce2005b97fab6c96e097a10f60b117
Binit Ghimire <thebinitghimire@gmail.com>
2022-02-09 10:59:37 +0545

    Little axios changes!

--- client/src/services/fetch.js
+++ client/src/services/fetch.js
@@ -1,28 +1,32 @@
 import axios from "axios";
 
+const requester = axios.create({
+  baseURL: "http://192.168.1.65:1337/api/"
+})
+
 export const getRequest = async (url, token) => {
-  const res = await axios.get(`http://localhost:1337/api/${url}`, {
+  const res = await requester.get(url, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const postRequest = async (url, post, token) => {
-  const res = await axios.post(`http://localhost:1337/api/${url}`, post, {
+  const res = await requester.post(url, post, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const putRequest = async (url, post, token) => {
-  const res = await axios.put(`http://localhost:1337/api/${url}`, post, {
+  const res = await requester.put(url, post, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const deleteRequest = async (url, token) => {
-  const res = await axios.delete(`http://localhost:1337/api/${url}`, {
+  const res = await requester.delete(url, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res;
