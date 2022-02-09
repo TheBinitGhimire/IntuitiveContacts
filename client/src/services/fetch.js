3db826fda2014ae4d6e7b4dff96f1a5a50eb6821
Binit Ghimire <thebinitghimire@gmail.com>
2022-02-09 11:16:29 +0545

    Heroku deployment successful!

--- client/src/services/fetch.js
+++ client/src/services/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const requester = axios.create({
-  baseURL: "http://192.168.1.65:1337/api/"
+  baseURL: "https://intuitive-contacts.herokuapp.com/api/"
 })
 
 export const getRequest = async (url, token) => {
